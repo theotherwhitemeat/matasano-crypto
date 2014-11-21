31ff55a0c67532ed5298fbe44d19782fd8bf9600
Kenny Ayers <theotherwhitemeat@gmail.com>
2014-11-21 00:41:28 -0800

    Removing cruft.

--- set1/set1.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-from cryptlib import *
-
-
-if __name__ == '__main__':
-	print("Hex to base64:",
-		hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
-
